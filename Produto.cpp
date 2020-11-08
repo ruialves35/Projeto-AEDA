db41e867bd103e8d0a37b52d8a20274d60d75e5e
ruialves35 <up201905853@fe.up.pt>
2020-11-08 10:02:06 +0000

    Adicionei metodos e comentarios ao produto

--- Produto.cpp
+++ Produto.cpp
@@ -1,56 +1,101 @@
 #include "Produto.h"
 
 /**
- * Constructor of derived Class
+ * Default Constructor of Produto
  * Sets to "" the nomeProduto and sets to 0 the stockFisico, stockOnline and valor
  */
 
 Produto::Produto(): nomeProduto(""), stockFisico(0), stockOnline(0), valor(0) {}
 
+/**
+ * Constructor of Produto
+ * @param nome name of Produto
+ * @param id identification of Produto (to be set by the Shop Owner)
+ * @param fisico stock fisico of Produto
+ * @param online stock online of Produto
+ * @param val value of Produto
+ */
 Produto::Produto(string nome, int id, int fisico, int online, double val): nomeProduto(nome),
                                                                            stockFisico(fisico), stockOnline(online), valor(val), id(id){}
 
 /**
- * sets nome, fisico and online
+ * sets Stock Fisico of Produto
+ * @param fisico new stock fisico
  */
-
 void Produto::setStockFisico(int fisico){
     stockFisico = fisico;
 }
 
+/**
+ * sets Stock Online of Produto
+ * @param online new stock online
+ */
 void Produto::setStockOnline(int online){
     stockOnline = online;
 }
 
+/**
+ * sets Nome of Produto
+ * @param nome new Produto's name
+ */
 void Produto::setNomeProduto(string nome){
     nomeProduto = nome;
 }
 
+/**
+ * sets Produto id
+ * @param newId new Produto's id
+ */
 void Produto::setId(int newId) {
     id = newId;
 }
 
+/**
+ * @return Produto's id
+ */
 int Produto::getId() const { return id; }
 
 /**
- * @return nome nomeProduto, fisico stockFisico, online stockOnline
+ * @return nome Produto's name
  */
-
 string Produto::getNomeProduto() const {return nomeProduto; }
 
+/**
+ * @return Produto's id
+ */
 int Produto::getStockFisico() const {return stockFisico;}
 
+/**
+ * @return stock of Produto in the Online Shop
+ */
 int Produto::getStockOnline() const {return stockOnline;}
 
+/**
+ * @return value of Produto
+ */
 double Produto::getValor() const {
     return valor;
 }
 
-
+/**
+ * overloading of operator == to compare 2 products.
+ * Products are equal if they have same id, nome, stock in online shop, stock in fisical shop and value.
+ * @param p1 product to compare
+ * @param p2 product to compare
+ * @return True if they are the same, false otherwise
+ */
 bool operator == (const Produto &p1, const Produto &p2){
-    return p1.id == p2.id;
+    return p1.id == p2.id && p1.nomeProduto == p2.nomeProduto && p1.stockOnline == p2.stockOnline &&
+           p1.stockFisico == p2.stockFisico && p1.valor == p2.valor;
 }
 
+/**
+ * overloading of operator < to compare 2 products.
+ * A product is smaller than other if his price is smaller than the price of the other
+ * @param p1 product to compare
+ * @param p2 product to compare
+ * @return True if value of p1 < p2
+ */
 bool operator < (const Produto &p1, const Produto &p2){
     return p1.valor < p2.valor;
 }
